593e94cb260bca32a05b945f0ed2ecefbcae4583
Dhanushpc <139443280+Dhanushpc@users.noreply.github.com>
2026-09-07 13:10:00 +0530

    Update and rename SpringdemooApplication.java to EmployeeManagementApplication.java

--- src-package/EmployeeManagementApplication.java
+++ src-package/EmployeeManagementApplication.java
@@ -1,4 +1,4 @@
-package com.example.springdemoo;
+package com.example.EmployeeManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication(scanBasePackages = "com.example")
 @EnableJpaRepositories(basePackages = "com.example.repository")
 @EntityScan(basePackages = "com.example.entity")
-public class SpringdemooApplication {
+public class EmployeeManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringdemooApplication.class, args);
+        SpringApplication.run(EmployeeManagementApplication.class, args);
     }
 }
